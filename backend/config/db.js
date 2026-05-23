4a9c17ecbee9c352fe2d3060bdc06c0f7eb67536
Utkarsh <buvautkarsh849@gmail.com>
2026-05-23 22:20:13 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const connString = process.env.MONGODB_URI || 'mongodb+srv://buvautkarsh849:wXMMIfWA6J6CEVA8@cluster0.g427upx.mongodb.net/solana-hunter?retryWrites=true&w=majority';
+    const connString = process.env.MONGODB_URI;
     
     const conn = await mongoose.connect(connString);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
